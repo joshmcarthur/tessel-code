2cf2b842bece3d5ca42d3fc513eb4189f8c06d86
Josh McArthur <joshua.mcarthur@gmail.com>
2014-07-11 09:26:36 +1200

    Add 'Hello World' from Tessel site

---
+++ blinky.js
@@ -0,0 +1,15 @@
+// Import Tessel library
+var tessel = require('tessel');
+
+// Set the pins as outputs with the initial
+// states. Truthy sets high, falsy set low.
+var led1 = tessel.led[0].output(1);
+var led2 = tessel.led[1].output(0);
+
+setInterval(function() {
+  console.log("I'm blinking. (Press CTRL+C to stop)");
+
+  // Toggle the LED states
+  led1.toggle();
+  led2.toggle();
+}, 100);
